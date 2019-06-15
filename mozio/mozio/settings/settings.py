fab5b23d8ce6220d8362af528574cc9b0ebd3984
Haviz Vaisal <havizvaisal@gmail.com>
2019-06-15 19:55:39 +0700

    Add STATIC_ROOT to settings

--- mozio/mozio/settings/settings.py
+++ mozio/mozio/settings/settings.py
@@ -133,7 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
 
 
 CACHES = {
